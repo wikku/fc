2bb4cc69c007499392922a03133d8a550c3c5cc5
Wiktor Kuchta <wiktorkuchta@protonmail.com>
2024-09-20 20:47:41 +0200

    watch.sh: cut off shortly after =====

--- abella/watch.sh
+++ abella/watch.sh
@@ -3,6 +3,7 @@
 while [[ 1 ]]
 do
 	clear
-	abella -i $1 < /dev/null
+	abella -i $1 < /dev/null \
+	| awk '/=====/{found=1; count=10} !found || count-- > 0 {print}'
 	inotifywait -e close_write $1 2>/dev/null
 done
